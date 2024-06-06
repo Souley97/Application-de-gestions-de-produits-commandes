c195c07bc603783f1ef6135799fd7fba4bda038c
souleymane97 <souleymane9700@gmail.com>
2024-06-06 18:48:37 +0000

    feat:Implémentation de l'inscription utilisateur.  Création du contrôleur d'inscription ,mise en place des validations pour les champs d'inscription

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(AuthController::class)->group(function(){
+
+    Route::get('/register','register')->name('register');
+    Route::post('/register/save','registerSave')->name('register.save');
+
 });
